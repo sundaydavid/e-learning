a292780ead03ea09675df3856a4f52f00dad8f9e
sundaydavid <sundayudoekong1@gmail.com>
2023-06-06 22:47:57 +0100

    Php Project

---
+++ footer.php
@@ -0,0 +1,8 @@
+<center>
+		<footer>
+		
+		<p>© Copyright 2023 E-Learning System: designed by the members of Group-1. PHP project</p>
+			<!-- <p>Programmed by: John Kevin Lorayna BSIS 4-A</p> -->
+		</footer>
+</center> 
+
---
+++ title_index.php
@@ -0,0 +1,37 @@
+<div class="row-fluid">
+
+	<div class="span12">
+
+	</div>
+
+</div>
+<div class="row-fluid">
+
+	<div class="span4">
+		<img class="index_logo" src="admin/images/logo2.png">
+	</div>
+	<div class="span8">
+
+		<div class="title">
+			<p class="chmsc">Federal polytechnic oko, Atani Campus</p>
+			<h3>
+
+				<p>E - Learning</p>
+
+			</h3>
+		</div>
+
+	</div>
+</div>
+
+<div class="row-fluid">
+
+	<div class="span12">
+		<br>
+		<div class="motto">
+			<p>FPO EXCELS:</p>
+			<p>Excellence, Competence and Educational</p>
+			<p>Leadership in Science and Technology</p>
+		</div>
+	</div>
+</div>
